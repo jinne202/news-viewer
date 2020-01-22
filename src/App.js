66657723b5a855985f0eb213dd66e0a25b8a8ce1
jinne202 <hanb5468@gmail.com>
2020-01-22 16:16:09 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async() => {
+    try {
+    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=au&apiKey=c60f3e16e5cb4a18964073ef0b5990d8');
+    setData(response.data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+  return(
+    <div>
+      <div>
+        <button onClick = {onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={30} value={JSON.stringify(data, null, 2)}/>}
     </div>
   );
-}
+};
 
 export default App;
